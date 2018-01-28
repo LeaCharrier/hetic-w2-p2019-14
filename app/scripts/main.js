194bc7fe46572de86660f237b740fc1c682b0046
Lea Charrier <lea.charrier@hetic.net>
2018-01-28 18:07:00 +0100

    get product \\ slider

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,24 +1,31 @@
 $(document).ready(function () {
- 	colorLunette();
+	sliderColorLunette();
 });
 
+var sliderColorLunette = function() {
+	$('.get-product__container-color-image').slick({
+	  slidesToShow: 1,
+	  arrows: false,
+	  slidesToScroll: 1,
+	  dots: true,
 
-var colorLunette = function() {
-	$('.get-product__container-color-choice img').click(function() {
-		if ($('.get-product__container-color-choice img').hasClass('active')) {
-			// Toggle class active button
-			$('.get-product__container-color-choice img').toggleClass('active', false);
-			$(this).addClass('active');
-			// This will have the category
-			var dataColor = $(this).data('color');
-			var allImg = $('.get-product__container-color-image');
-
-			// Current item is should be active
-	    var $currImg = $('.get-product__container-color-image[data-color=' + dataColor + ']');
-	    $currImg.addClass('active');
-	    // Remove the active class for other items
-    	allImg.not($currImg).removeClass('active');
-
-		}
-	});
+	  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+      	slidesToShow: 1,
+        centerMode: true,
+	  		centerPadding: '30px',
+      }
+    },
+    {
+      breakpoint: 500,
+      settings: {
+      	slidesToShow: 1,
+      	centerMode: true,
+	  		centerPadding: '30px',
+      }
+    },
+   ]
+  });
 }
