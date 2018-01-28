ea8e89059f362a4b5fc6f437028fa66bcd4c416d
Lea Charrier <lea.charrier@hetic.net>
2018-01-28 17:04:59 +0100

    get product \\ js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,24 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function () {
+ 	colorLunette();
+});
+
+
+var colorLunette = function() {
+	$('.get-product__container-color-choice img').click(function() {
+		if ($('.get-product__container-color-choice img').hasClass('active')) {
+			// Toggle class active button
+			$('.get-product__container-color-choice img').toggleClass('active', false);
+			$(this).addClass('active');
+			// This will have the category
+			var dataColor = $(this).data('color');
+			var allImg = $('.get-product__container-color-image');
+
+			// Current item is should be active
+	    var $currImg = $('.get-product__container-color-image[data-color=' + dataColor + ']');
+	    $currImg.addClass('active');
+	    // Remove the active class for other items
+    	allImg.not($currImg).removeClass('active');
+
+		}
+	});
+}
