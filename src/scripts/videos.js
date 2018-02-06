d62093b095dd24d96133e4108bdb086a3933b295
Lea Charrier <lea.charrier@hetic.net>
2018-02-06 16:15:48 +0100

    section video

--- src/scripts/videos.js
+++ src/scripts/videos.js
@@ -1,10 +1,8 @@
 export function videos() {
 
-  var videos = document.querySelectorAll(".presentationProduct__videos__wraper");
-  console.log(videos);
+  var videos = document.querySelectorAll(".presentationProductVideos__wraper");
   for (let i = 0; i < videos.length; i++) {
     videos[i].addEventListener('click', function () {
-      console.log("working");
       videos[i].style.width = "100%"
     })
   }
