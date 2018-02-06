254232e0b7478accc6433de5ab2b8df5eb666615
Lea Charrier <lea.charrier@hetic.net>
2018-02-06 18:56:56 +0100

    section image

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,6 @@
 import { sliderColorLunette } from './slick.js';
 import { sliderKitLunette } from './slick.js';
-import { videos } from './videos.js';
 
-videos();
+
 sliderColorLunette();
 sliderKitLunette();
--- src/scripts/videos.js
+++
@@ -1,9 +0,0 @@
-export function videos() {
-
-  var videos = document.querySelectorAll(".presentationProductVideos__wraper");
-  for (let i = 0; i < videos.length; i++) {
-    videos[i].addEventListener('click', function () {
-      videos[i].style.width = "100%"
-    })
-  }
-}
