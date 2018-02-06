5d8b03d576da5094963dd3173029499e7fbf8f3e
ClementPicot <clement.picot@outlook.fr>
2018-02-06 22:12:03 +0100

    ajout animation mobile non responsive

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,33 @@
 import { sliderColorLunette } from './slick.js';
 import { sliderKitLunette } from './slick.js';
+import { animation } from './animation.js';
+
+
+let global = document.querySelector(".container-global");
+let lunette = document.querySelector(".lunettes");
+let anim = document.querySelector(".animation-block");
+
+var fixedControl = function(){
+    let actualScroll = window.pageYOffset + 250;
+    let elementTop = global.offsetTop;
+
+    if (actualScroll >= elementTop && !anim.classList.contains('mr-scroll')){
+        anim.style.left = document.querySelector('.lunettes').offsetLeft;
+        anim.classList += " mr-scroll";
+    }
+    else if ( actualScroll < elementTop && anim.classList.contains('mr-scroll')){
+        anim.classList.remove('mr-scroll');
+    }
+
+}
+
+
+
+window.addEventListener('scroll', fixedControl);
+window.addEventListener('resize', fixedControl);
+
 
 
 sliderColorLunette();
 sliderKitLunette();
+animation();
